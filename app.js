e67a4f5c3d0cbe2c675da9f1ff992f7501bfef19
Eli <elimermz@gmail.com>
2026-07-13 09:17:05 +0300

    wrote update status route

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import { fileURLToPath } from "url";
 
 const app = express();
 const ORDERS_FILE_PATH = "./orders.json";
+const statuses = ["NEW", "PREPARING", "READY", "DELIVERED", "CANCELLED"];
+const statusTransition = {"NEW" : ["PREPARING", CANCELLED], "PREPARING": ["READY", "CANCELLED"], "READY": ["DELIVERED"]}
 
 app.use(express.json());
 
@@ -36,7 +38,7 @@ app.post("/orders", async (req, res, next) => {
 });
 
 app.get("/orders", async (req, res, next) => {
-  const statuses = ["NEW", "PREPARING", "READY", "DELIVERED", "CANCELLED"];
+
   const { statusType, customer, tableNum } = req.query;
   const orders = await getFile(ORDERS_FILE_PATH);
 
@@ -92,6 +94,56 @@ app.get("/orders/:id", async (req, res, next) => {
     return next(error);
   }
   res.status(200).json({ success: true, data: order });
+});
+
+app.patch("/orders/:id/status", (req, res, next) => {
+  const orderId = Number(req.params.id);
+
+  if (isNaN(orderId)) {
+    const error = new Error("invalid id");
+    error.status = 400;
+    return next(error);
+  }
+
+  let newStatus = req.body.status;
+
+  if (newStatus){
+    newStatus = newStatus.toUpperCase()
+      if (!statuses.includes(newStatus)){
+        const error = new Error("invalid status to update");
+        error.status = 400;
+        return next(error);
+      }
+  }
+  else{
+        const error = new Error("missing status to update");
+        error.status = 400;
+        return next(error);
+  }
+
+
+
+  const orders = await getFile(ORDERS_FILE_PATH);
+  const order = orders.find((order) => order.id === orderId);
+
+  if (!order) {
+    const error = new Error("order not found");
+    error.status = 404;
+    return next(error);
+  }
+
+  const allowedUpdate = statusTransition[order.status];
+
+  if (!allowedUpdate.includes(newStatus)){
+    const error = new Error("invalid status transition");
+    error.status = 400;
+    return next(error);
+  }
+
+  order.status = newStatus
+
+  await saveFile(ORDERS_FILE_PATH, orders);
+  res.status(200).json({success: true, message: "status updated successfully"})
 });
 
 app.use((err, req, res, next) => {
